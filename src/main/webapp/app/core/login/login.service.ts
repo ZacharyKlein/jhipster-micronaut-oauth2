24fbdf8f0d4ae9f4b259f9f8309b2eb2b02fdd40
Zachary Klein <zak@silver-chalice.com>
2020-05-06 12:05:00 -0500

    Remove logout code

--- src/main/webapp/app/core/login/login.service.ts
+++ src/main/webapp/app/core/login/login.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Location } from '@angular/common';
 
 import { AuthServerProvider } from 'app/core/auth/auth-session.service';
-import { Logout } from './logout.model';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
@@ -13,18 +12,6 @@ export class LoginService {
   }
 
   logout(): void {
-    this.authServerProvider.logout().subscribe((logout: Logout) => {
-      let logoutUrl = logout.logoutUrl;
-      const redirectUri = `${location.origin}${this.location.prepareExternalUrl('/')}`;
-
-      // if Keycloak, uri has protocol/openid-connect/token
-      if (logoutUrl.includes('/protocol')) {
-        logoutUrl = logoutUrl + '?redirect_uri=' + redirectUri;
-      } else {
-        // Okta
-        logoutUrl = logoutUrl + '?id_token_hint=' + logout.idToken + '&post_logout_redirect_uri=' + redirectUri;
-      }
-      window.location.href = logoutUrl;
-    });
+    this.authServerProvider.logout().subscribe(() => {});
   }
 }
